240ce5365c0a410db5745d863d81f291a08e02c1
Sunil Kumar <neil001002@gmail.com>
2021-06-12 18:18:00 +0530

    webview

--- components/NewsCard.tsx
+++ components/NewsCard.tsx
@@ -18,13 +18,16 @@ const NewsCard = ({ item }) => {
 
   const navigation = useNavigation();
 
-  const defaultImage = require("../assets/icons/briefcase.png")
+  const defaultImage = require("../assets/icons/briefcase.png");
 
   return (
     <View style={styles.cardView}>
       <Text style={styles.title}>{item.title}</Text>
       <Text style={styles.author}>{item.source.name}</Text>
-      <Image style={styles.image} source={{ uri: item.urlToImage ? item.urlToImage : defaultImage }} />
+      <Image
+        style={styles.image}
+        source={{ uri: item.urlToImage ? item.urlToImage : defaultImage }}
+      />
       <Text style={styles.author}> {time} </Text>
       <TouchableOpacity
         onPress={() => navigation.navigate("Webview", { url: item.url })}
@@ -37,7 +40,7 @@ const NewsCard = ({ item }) => {
 
 const styles = StyleSheet.create({
   cardView: {
-    elevation: 2,
+    elevation: 12,
     backgroundColor: "white",
     margin: width * 0.03,
     borderRadius: width * 0.05,
--- screens/Webview.tsx
+++ screens/Webview.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { HeaderTab } from "../components";
+import { HeaderTab, HeaderTabIcons } from "../components";
 import { WebView } from "react-native-webview";
 
 const Webview = ({ route }) => {
   return (
     <View style={{ flex: 1 }}>
-      <WebView source={{ uri: route.params.url }} />
+      <View
+        style={{
+          position: "absolute",
+          left: 0,
+          right: 0,
+          width: "100%",
+          zIndex: 1,
+        }}
+      >
+        <HeaderTabIcons title={"News"} />
+      </View>
+      <WebView source={{ uri: route.params.url }} style={{ marginTop: 50 }} />
     </View>
   );
 };
