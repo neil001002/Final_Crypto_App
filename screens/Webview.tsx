3caa2f175d6f622349b9eab0715bd42b5c59992c
Sunil Kumar <neil001002@gmail.com>
2021-06-12 21:09:18 +0530

    progress bar in webview

--- screens/Webview.tsx
+++ screens/Webview.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
-import { HeaderTab, HeaderTabIcons } from "../components";
+import { HeaderTabIcons } from "../components";
 import { WebView } from "react-native-webview";
+import * as Progress from "react-native-progress";
 
 const Webview = ({ route }) => {
+  const [progress, setProgress] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+
   return (
     <View style={{ flex: 1 }}>
       <View
@@ -16,8 +20,24 @@ const Webview = ({ route }) => {
         }}
       >
         <HeaderTabIcons title={"News"} />
+        {!isLoaded ? (
+          <Progress.Bar
+            progress={progress}
+            width={null}
+            borderWidth={0}
+            borderRadius={0}
+            color="orange"
+          />
+        ) : null}
       </View>
-      <WebView source={{ uri: route.params.url }} style={{ marginTop: 50 }} />
+      <WebView
+        source={{ uri: route.params.url }}
+        style={{ marginTop: 50 }}
+        //progress bar goaway
+        onLoadEnd={() => setLoaded(true)}
+        //progress bar
+        onLoadProgress={({ nativeEvent }) => setProgress(nativeEvent.progress)}
+      />
     </View>
   );
 };
--- stores/newsCryptoAPI/newsActions.tsx
+++ stores/newsCryptoAPI/newsActions.tsx
@@ -10,47 +10,44 @@ export const GET_COIN_NEWS_FAILURE = "GET_COIN_NEWS_FAILURE";
 // const apiKey1 = "f08f0a610915445b89739684b1217bc4"
 // const apiKey2 = "8e4d3bae661342c79dcbb9ad683a7095"
 
-
 export const getCoinNewsBegin = () => ({
-    type: GET_COIN_NEWS_BEGIN,
+  type: GET_COIN_NEWS_BEGIN,
 });
 
 export const getCoinNewsSuccess = (coinsnews) => ({
-    type: GET_COIN_NEWS_SUCCESS,
-    payload: { coinsnews },
+  type: GET_COIN_NEWS_SUCCESS,
+  payload: { coinsnews },
 });
 
 export const getCoinNewsFailure = (error) => ({
-    type: GET_COIN_NEWS_FAILURE,
-    payload: { error },
+  type: GET_COIN_NEWS_FAILURE,
+  payload: { error },
 });
 
-export function getCoinNews(
-    { ID }
-) {
-    return (dispatch) => {
-        dispatch(getCoinNewsBegin());
+export function getCoinNews({ ID }) {
+  return (dispatch) => {
+    dispatch(getCoinNewsBegin());
 
-        let apiUrl = `https://newsapi.org/v2/everything?q=${ID}&sortBy=publishedAt&language=en&apiKey=f08f0a610915445b89739684b1217bc4`;
+    let apiUrl = `https://newsapi.org/v2/everything?q=${ID}&sortBy=publishedAt&language=en&apiKey=8e4d3bae661342c79dcbb9ad683a7095`;
 
-        return axios({
-            url: apiUrl,
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-            },
-        })
-            .then((response) => {
-                console.log("getCoinNews");
-                console.log(response);
-                if (response.status == 200) {
-                    dispatch(getCoinNewsSuccess(response.data));
-                } else {
-                    dispatch(getCoinNewsFailure(response.data));
-                }
-            })
-            .catch((error) => {
-                dispatch(getCoinNewsFailure(error));
-            });
-    };
+    return axios({
+      url: apiUrl,
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        console.log("getCoinNews");
+        console.log(response);
+        if (response.status == 200) {
+          dispatch(getCoinNewsSuccess(response.data));
+        } else {
+          dispatch(getCoinNewsFailure(response.data));
+        }
+      })
+      .catch((error) => {
+        dispatch(getCoinNewsFailure(error));
+      });
+  };
 }
