a37a4ea8abe0f7a4a14609468155ef48e0227e30
Pankaj Kumar <pankajkumardas727@gmail.com>
2021-06-16 16:23:35 +0530

    changed app name

--- screens/more_screens/AboutUs.tsx
+++ screens/more_screens/AboutUs.tsx
@@ -11,7 +11,12 @@ const AboutUs = () => {
                     margin: SIZES.padding,
                 }}
             >
-                <Text style={{ fontWeight: "600", fontSize: SIZES.h1 }}>AboutUs</Text>
+                <Text style={{ fontWeight: "600", fontSize: SIZES.h1 }}>About Us</Text>
+                <View>
+                    <Text style={{ fontSize: SIZES.h2, marginVertical: 4 }}>Dear users!</Text>
+                    <Text style={{ fontSize: SIZES.h3, marginVertical: 4 }}>We have prepared a detailed review of the Crypto News app, which includes the features from the latest update which took place by the end of 2019. The app got many new features with the update and we hope that all of them will be useful.</Text>
+                    <Text style={{ fontSize: SIZES.h3, marginVertical: 4 }}>If you have any suggestions or comments, please write to info.cryptonium@gmail.com.</Text>
+                </View>
             </View>
         </ScrollView>
     )
